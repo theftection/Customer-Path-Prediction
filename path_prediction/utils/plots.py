e3338f4e2840444c1d77f6c42f01b9279c6a9cbb
Joshua Kraft <nathan.joshua.kraft@gmx.de>
2023-01-24 16:12:18 +0100

    adds idea to visulize transition net

--- path_prediction/utils/plots.py
+++ path_prediction/utils/plots.py
@@ -5,3 +5,10 @@ def draw_predicted_path(im0, path, color, line_thickness):
         org = path[i].get_original_coordinates()
         dest = path[i+1].get_original_coordinates()
         cv2.arrowedLine(im0, org, dest, color, line_thickness)
+
+def draw_transition_net(im0, transition_net, color):
+    # draw the grid on the image
+    for i in range(transition_net.shape[0]):
+        ...
+
+# Idea: make a 5x5 heatmap for every cell where a person is standing on
