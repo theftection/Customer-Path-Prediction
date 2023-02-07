0e298cf05ad6f946f90d3ef7b2c831bdf3d79d65
Joshua Kraft <nathan.joshua.kraft@gmx.de>
2023-02-07 19:25:18 +0100

    adds functions to estimate position based of a bounding box

--- path_prediction/perspective_transformation/transform_perspektive.py
+++ path_prediction/perspective_transformation/transform_perspektive.py
@@ -22,25 +22,85 @@ def project_2D_to_3D(P_inv, camera_origin, point_2D, height):
     world_point = camera_origin + ray_3D * t
     return world_point.astype(int)
 
+def top_center(bbox):
+    """Get top center of bounding box.
+    Args:
+        bbox (list): Bounding box.
+    Returns:
+        np.ndarray: Top center of bounding box.
+    """
+    x1, y1, x2, y2 = bbox
+    return np.array([(x1 + x2) / 2, y1])
 
-def bb_2_map():
-    ...
+def bottom_center(bbox):
+    """Get bottom center of bounding box.
+    Args:
+        bbox (list): Bounding box.
+    Returns:
+        np.ndarray: Bottom center of bounding box.
+    """
+    x1, y1, x2, y2 = bbox
+    return np.array([(x1 + x2) / 2, y2])
 
-def bb_2_heigth():
-    ...
+def check_redzones(point, redzones):
+    for zone in redzones:
+        if point[0] > zone[0] and point[0] < zone[2] and point[1] > zone[1] and point[1] < zone[3]:
+            return True, zone
+    return False, None
 
-def red_zone_estimation():
-    ...
+def correct_for_redzone(standing_position, ray_3D, redzones):
+        standing_position = standing_position[0]
+        in_redzone, zone = check_redzones(standing_position, redzones)
+        if in_redzone:
+            t_x = (zone[2] - standing_position[0]) / ray_3D[:, 0]
+            t_y = (zone[3] - standing_position[1]) / ray_3D[:, 1]
+            t_x_neg = (zone[0] - standing_position[0]) / ray_3D[:, 0]
+            t_y_neg = (zone[1] - standing_position[1]) / ray_3D[:, 1]
+            # take the t which has the smallest absolute value but keep the sign
+            t = [t_x, t_y, t_x_neg, t_y_neg][np.argmin(np.abs([t_x, t_y, t_x_neg, t_y_neg]))]
+            standing_position = standing_position + ray_3D * t
+        return standing_position
+
+def estimate_floor_position(P_inv, camera_origin, bbox, redzones, avg_heigth=100):
+    """Estimate floor position of person by checking how tall this
+    person would be if the bbox would show the full person. If this
+    is not the case take the head position as the estimate.
+    """
+    bottom_center_2D = np.array([bottom_center(bbox)])
+    bottom_center_2D_h = np.concatenate((bottom_center_2D, np.ones((bottom_center_2D.shape[0],1))), axis=1)
+    bottom_center_3D_h = P_inv @ bottom_center_2D_h.T
+    bottom_center_3D = (bottom_center_3D_h / bottom_center_3D_h[3])[:3].T
+    bottom_ray_3D = bottom_center_3D - camera_origin
+    bottom_t = (0 - camera_origin[2]) / bottom_ray_3D[:, 2]
+    theo_standing_position = camera_origin + bottom_ray_3D * bottom_t
+    depth_standing_position = theo_standing_position[0, 1]
+
+    top_center_2D = np.array([top_center(bbox)])
+    top_center_2D_h = np.concatenate((top_center_2D, np.ones((top_center_2D.shape[0],1))), axis=1)
+    top_center_3D_h = P_inv @ top_center_2D_h.T
+    top_center_3D = (top_center_3D_h / top_center_3D_h[3])[:3].T
+    top_ray_3D = top_center_3D - camera_origin
+    top_t = (depth_standing_position - camera_origin[1]) / top_ray_3D[:, 1]
+    top_center_world = camera_origin + top_ray_3D * top_t
+
+    # check if theoretical height is large enough
+    # to assume the whole person is in the frame
+    if top_center_world[0, 2] > 100 or "bottom_center" in "greenzone":
+        return correct_for_redzone(theo_standing_position, bottom_ray_3D, redzones).astype(int)
+    else:
+        head_t = (avg_heigth - camera_origin[2]) / top_ray_3D[:, 2]
+        head_standing_position = camera_origin + top_ray_3D * head_t
+        return correct_for_redzone(head_standing_position, bottom_ray_3D, redzones).astype(int)
 
 
 if __name__ == "__main__":
     # load points
-    project = "test_impl"
+    project = "Ch4_demo_960"
     P, P_inv, camera_origin = load_projection_matrix(project)
-    points_2D = np.array([[0, 0]])
-    points_3D = project_2D_to_3D(P_inv, camera_origin, points_2D, 0)
-    print(points_3D)
-
+    redzones = np.array([[0, 0, 230, 400], 
+                        [10, 15, 20, 25]])
+    bbox = [0, 0, 100, 100]
+    print(estimate_floor_position(P_inv, camera_origin, bbox, redzones))
 
 
 
