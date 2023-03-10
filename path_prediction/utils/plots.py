3efdf24cb8b2b357fe72651e9bfa751a1977e3a8
OshFrosch <nathan.joshua.kraft@gmx.de>
2023-03-10 16:08:34 +0100

    adds function to plot emoji on floor plan

--- path_prediction/utils/plots.py
+++ path_prediction/utils/plots.py
@@ -14,3 +14,33 @@ def draw_transition_net(im0, transition_net, color):
         ...
 
 # Idea: make a 5x5 heatmap for every cell where a person is standing on
+
+def draw_icon(img, icon, x, y):
+
+    image = img.copy()
+
+    if icon.shape[2] == 4:
+        alpha = icon[:,:,3]
+        mask = cv2.merge((alpha,alpha,alpha))
+        icon = icon[:,:,0:3]
+
+    icon_size = icon.shape[:2]
+    icon = cv2.resize(icon, icon_size)
+
+    x_offset = max(0, x - int(icon_size[0]/2))
+    y_offset = max(0, y - int(icon_size[1]/2))
+
+    roi = image[y_offset:y_offset+icon_size[1], x_offset:x_offset+icon_size[0]]
+
+    icon_gray = cv2.cvtColor(icon, cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(icon_gray, 5, 255, cv2.THRESH_BINARY)
+    mask_inv = cv2.bitwise_not(mask)
+
+    roi_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+
+    icon_fg = cv2.bitwise_and(icon, icon, mask=mask)
+    # Combine the icon and ROI to get the final image
+    dst = cv2.add(roi_bg, icon_fg)
+    image[y_offset:y_offset+icon_size[1], x_offset:x_offset+icon_size[0]] = dst
+
+    return image
